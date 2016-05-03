ce9d47ba0f038c80b4a9faeb9c34e1b22f00caad
Alex Kalinin <login.hedin@gmail.com>
2016-05-03 15:22:28 +0600

    done

--- main.js
+++ main.js
@@ -1,128 +1,20 @@
-var obj1 = [
-    {
-        "date": "2000",
-        "value": 2025
-    }, {
-        "date": "2001",
-        "value": 1882
-    }, {
-        "date": "2002",
-        "value": 1809
-    }, {
-        "date": "2003",
-        "value": 1322
-    }, {
-        "date": "2004",
-        "value": 1122
-    }, {
-        "date": "2005",
-        "value": 1114
-    }, {
-        "date": "2006",
-        "value": 984
-    }, {
-        "date": "2007",
-        "value": 711
-    }, {
-        "date": "2007",
-        "value": 665
-    }, {
-        "date": "2008",
-        "value": 580
-    }, {
-        "date": "2009",
-        "value": 443
-    }, {
-        "date": "2010",
-        "value": 441
-    }, {
-        "date": "2011",
-        "value": 395
-    }
-];
-
-var obj2 = [
-    {
-        "desc": "USA",
-        "code": 2800,
-        "date": "2000"
-    },
-    {
-        "desc": "USA",
-        "code": 1500,
-        "date": "2005"
-    },
-    {
-        "desc": "USA",
-        "code": 290,
-        "date": "2005"
-    },
-    {
-        "desc": "USA",
-        "code": 3800,
-        "date": "2007"
-    },
-    {
-        "desc": "USA",
-        "code": 3214,
-        "date": "2007"
-    },
-    {
-        "desc": "USA",
-        "code": 2650,
-        "date": "2007"
-    },
-    {
-        "desc": "USA",
-        "code": 2840,
-        "date": "2007"
-    },
-    {
-        "desc": "USA",
-        "code": 2600,
-        "date": "2005"
-    },
-    {
-        "desc": "USA",
-        "code": 2700,
-        "date": "2000"
-    },
-    {
-        "desc": "USA",
-        "code": 1800,
-        "date": "2000"
-    },
-    {
-        "desc": "USA",
-        "code": 12800,
-        "date": "2000"
-    }
-];
-
-var obj3 = $.getJSON('graph2.json', function(data) {
-    var chart = AmCharts.makeChart( "chartdiv", buildbaseObj(data) );
+$.getJSON('data/graph1.json', function(data) {
+    var chart = AmCharts.makeChart("chartdiv", buildGraph1(data) );
 
 });
 
-
-
-
-var obj2 = $.getJSON('graph2.json', function(data) {
-    return data;
-
-});
-
-function filterObj2ByDate(date){
+function filterDataByDate(filterDate, obj){
+    obj = obj || [];
     var data = [];
-    for(var i = 0; i < obj2.length; i++){
-        if(obj2[i].date === date){
-            data.push({code: obj2[i].code, desc: obj2[i].desc});
+    for(var i = 0; i < obj.length; i++){
+        if(obj[i].date === filterDate){
+            data.push({code: obj[i].code, desc: obj[i].desc});
         }
     }
     return data;
 }
 
-function buildnewObj(data){
+function buildGraph2(data){
     var newObj =  {
         "type": "serial",
         "theme": "light",
@@ -156,8 +48,7 @@ function buildnewObj(data){
 
 }
 
-function buildbaseObj(obj1){
-    debugger;
+function buildGraph1(obj1){
     var baseObj =  {
         "type": "serial",
         "dataProvider": obj1,
@@ -187,13 +78,13 @@ function buildbaseObj(obj1){
             "event": "clickGraphItem",
             "method": function(e) {
                 var date = e.item.category;
-                var data = filterObj2ByDate(date);
-                var newObj = buildnewObj(data);
-
-                var chart = AmCharts.makeChart( "chartdiv2", newObj );
-
                 var text = e.item.category + " (value: " + e.item.values.value + ")";
                 document.getElementById("clicked").innerHTML = text;
+
+                $.getJSON('data/graph2.json', function(obj) {
+                    var data = filterDataByDate(date, obj);
+                    var chart2 = AmCharts.makeChart( "chartdiv2", buildGraph2(data));
+                });
             }
         }]
 
